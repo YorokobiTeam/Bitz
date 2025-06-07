0295358826771eeb2d0348590eac45eb2dfa8fae
Maik <maikpla3@gmail.com>
2025-06-08 05:38:09 +0700

    Added textures and home UI

---
+++ Assets/UI/MainMenuController.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UIElements;
+
+public class MainMenuController : MonoBehaviour
+{
+    private List<VisualElement> leftPanelElements = new List<VisualElement>();
+    private List<VisualElement> rightPanelElements = new List<VisualElement>();
+
+}
