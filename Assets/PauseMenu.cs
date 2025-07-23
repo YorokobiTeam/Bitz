97672386b28c69364cf2b9f5e6feca5496f3dfec
TuaaanAnhNguyen <ntuananh2503@gmail.com>
2025-07-23 13:36:40 +0700

    Revert "added audio mixer + keybinding in setting screen"
    This reverts commit 6bbe6780e24059dd690c81e7de72057300b5855c.

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,52 +1,31 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class PauseMenu : MonoBehaviour
 {
     public GameObject pauseMenu;
-    public GameObject settingsScreen;
+
     public UIController UIControllerInstance;
+
     public static bool isPaused = false;
 
-    public Button resumeButton;
-    public Button restartButton;
-    public Button settingsButton;
-    public Button exitButton;
-
+    private UIDocument uiDocument;
     private List<Button> menuButtons = new List<Button>();
     private int currentButtonIndex = 0;
-
-    private float selectedScale = 1.2f;
-    private float defaultScale = 1f;
-    private float animationDuration = 0.2f;
-    private Color selectedColor = Color.white;
-    private Color defaultColor = Color.black;
-    private Color selectedBackgroundColor = new Color(0.23f, 0.23f, 0.23f, 0.7f); 
-    private Color defaultBackgroundColor = new Color(0, 0, 0, 0);
-
-    private void Awake()
-    {
-        if (resumeButton != null) menuButtons.Add(resumeButton);
-        if (restartButton != null) menuButtons.Add(restartButton);
-        if (settingsButton != null) menuButtons.Add(settingsButton);
-        if (exitButton != null) menuButtons.Add(exitButton);
-
-    }
 
     public void Start()
     {
         pauseMenu.SetActive(false);
-        settingsScreen.SetActive(false);
-        Debug.Log("PauseMenu initialized. Press Escape to toggle pause menu.");
+        uiDocument = GetComponent<UIDocument>();
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Escape) && !settingsScreen.activeSelf)
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
-            Debug.Log("Escape key pressed. Toggling pause menu.");
             if (isPaused)
             {
                 ResumeGame();
@@ -59,39 +38,39 @@ public class PauseMenu : MonoBehaviour
 
     }
 
-    //private void OnNavigate(NavigationMoveEvent evt)
-    //{
-    //    if (!isPaused) return;
+    private void OnNavigate(NavigationMoveEvent evt)
+    {
+        if (!isPaused) return;
 
-    //    switch (evt.direction)
-    //    {
-    //        case NavigationMoveEvent.Direction.Up:
-    //            currentButtonIndex = WrapIndex(--currentButtonIndex, 0, menuButtons.Count - 1);
-    //            break;
-    //        case NavigationMoveEvent.Direction.Down:
-    //            currentButtonIndex = WrapIndex(++currentButtonIndex, 0, menuButtons.Count - 1);
-    //            break;
-    //        case NavigationMoveEvent.Direction.None:
-    //            break;
-    //    }
-    //    menuButtons[currentButtonIndex].Focus();
-    //    UpdateButtonSelection();
-    //}
-    //private void UpdateButtonSelection()
-    //{
-    //    for (int i = 0; i < menuButtons.Count; i++)
-    //    {
-    //        if (i == currentButtonIndex)
-    //        {
-    //            menuButtons[i].AddToClassList("selected");
-    //            menuButtons[i].Focus();
-    //        }
-    //        else
-    //        {
-    //            menuButtons[i].RemoveFromClassList("selected");
-    //        }
-    //    }
-    //}
+        switch (evt.direction)
+        {
+            case NavigationMoveEvent.Direction.Up:
+                currentButtonIndex = WrapIndex(--currentButtonIndex, 0, menuButtons.Count - 1);
+                break;
+            case NavigationMoveEvent.Direction.Down:
+                currentButtonIndex = WrapIndex(++currentButtonIndex, 0, menuButtons.Count - 1);
+                break;
+            case NavigationMoveEvent.Direction.None:
+                break;
+        }
+        menuButtons[currentButtonIndex].Focus();
+        UpdateButtonSelection();
+    }
+    private void UpdateButtonSelection()
+    {
+        for (int i = 0; i < menuButtons.Count; i++)
+        {
+            if (i == currentButtonIndex)
+            {
+                menuButtons[i].AddToClassList("selected");
+                menuButtons[i].Focus();
+            }
+            else
+            {
+                menuButtons[i].RemoveFromClassList("selected");
+            }
+        }
+    }
 
     public void PauseGame()
     {
@@ -99,7 +78,7 @@ public class PauseMenu : MonoBehaviour
         pauseMenu.SetActive(true);
         isPaused = true;
         currentButtonIndex = 0;
-        //UIControllerInstance.musicPlayer.Pause();
+        UIControllerInstance.musicPlayer.Pause(); 
 
     }
 
@@ -108,23 +87,22 @@ public class PauseMenu : MonoBehaviour
         Time.timeScale = 1f;
         pauseMenu.SetActive(false);
         isPaused = false;
-        //UIControllerInstance.musicPlayer.UnPause();
+        UIControllerInstance.musicPlayer.UnPause();
     }
 
     public void OpenSettings()
     {
         Debug.Log("Openning Settings...");
-        settingsScreen.SetActive(true);
     }
     public void ExitGame()
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 1f; 
         SceneManager.LoadScene("MainMenu");
     }
 
     public void RestartGame()
     {
-        Time.timeScale = 1f;
+        Time.timeScale = 1f; 
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
     private int WrapIndex(int indx, int min, int max)
